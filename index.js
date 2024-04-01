6e8670dd818eb5ce0fe2865637a1e0027022ef06
champer3 <okunolas21@gmail.com>
2024-04-01 16:06:50 -0500

    updated the name

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
+    // useNewUrlParser: true,
     dbName: 'RoomService'
     // useCreateIndex: true,
     // useFindAndModify: false
