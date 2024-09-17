85eec00fc3228af7da3da81d50222653c3d7f4ab
champer3 <okunolas21@gmail.com>
2024-09-17 00:27:52 -0500

    order enum update

--- Models/orderModel.js
+++ Models/orderModel.js
@@ -28,7 +28,7 @@ const orderSchema = new mongoose.Schema(
     },
     orderStatus: {
       type: String,
-      enum: ['Ordered', 'Preparing', 'Out for Delivery', 'Delivered', 'Cancelled'],
+      enum: ['Ordered', 'Ready for Deliver', 'Out for Delivery', 'Delivered', 'Cancelled'],
       default: "Ordered",
     },
     paymentMethod: {
